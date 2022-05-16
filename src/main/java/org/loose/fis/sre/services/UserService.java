d3cd4283c07dcc87464991b82b9bbb2c0c610108
andrei2427 <andrei.paulescu@student.upt.ro>
2022-05-16 14:37:36 +0300

    Basic Structure

--- src/main/java/org/loose/fis/sre/services/UserService.java
+++ src/main/java/org/loose/fis/sre/services/UserService.java
@@ -14,28 +14,10 @@ import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
 
 public class UserService {
 
-    private static ObjectRepository<User> userRepository;
-
-    public static void initDatabase() {
-        Nitrite database = Nitrite.builder()
-                .filePath(getPathToFile("Events_App.db").toFile())
-                .openOrCreate("test", "test");
-
-        userRepository = database.getRepository(User.class);
-    }
-
     public static void addUser(String username, String password, String role) throws UsernameAlreadyExistsException {
         checkUserDoesNotAlreadyExist(username);
         userRepository.insert(new User(username, encodePassword(username, password), role));
     }
-
-    private static void checkUserDoesNotAlreadyExist(String username) throws UsernameAlreadyExistsException {
-        for (User user : userRepository.find()) {
-            if (Objects.equals(username, user.getUsername()))
-                throw new UsernameAlreadyExistsException(username);
-        }
-    }
-
     private static String encodePassword(String salt, String password) {
         MessageDigest md = getMessageDigest();
         md.update(salt.getBytes(StandardCharsets.UTF_8));
