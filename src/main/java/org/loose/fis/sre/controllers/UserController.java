5ac4a008ca48fe121b3536fed72f2da6d89d0f2e
andrei2427 <andrei.paulescu@student.upt.ro>
2022-05-18 13:36:30 +0300

    EV-21 <message>

--- src/main/java/org/loose/fis/sre/controllers/UserController.java
+++ src/main/java/org/loose/fis/sre/controllers/UserController.java
@@ -10,12 +10,21 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.loose.fis.sre.model.Event;
 import org.loose.fis.sre.model.User;
 import org.loose.fis.sre.services.UserService;
 
 public class UserController {
+    @FXML
+    private Line l1;
+    @FXML
+    private Line l2;
+    @FXML
+    private Line l3;
+    @FXML
+    private Line l4;
     @FXML
     private Label select;
     @FXML
@@ -32,6 +41,7 @@ public class UserController {
     @FXML
     public void initialize(){
         Tabel.setVisible(false);
+        l1.setVisible(false);l2.setVisible(false);l3.setVisible(false);l4.setVisible(false);
         Name.setCellValueFactory(new PropertyValueFactory<Event, String>("name"));
         Place.setCellValueFactory(new PropertyValueFactory<Event, String>("loc"));
         Data.setCellValueFactory(new PropertyValueFactory<Event, String>("data"));
@@ -53,12 +63,16 @@ public class UserController {
         records.add(new Event("articol","MH","22.02.2002",10));
         Tabel.setItems(records);
         Tabel.setVisible(true);
+        l1.setVisible(true);l2.setVisible(true);l3.setVisible(true);l4.setVisible(true);
     }
     @FXML
     public void getSelection(){
         Event ev = Tabel.getSelectionModel().getSelectedItem();
         String txt = "Selected: " + ev.toString();
         select.setText(txt);
+    }
+    public void MakeReservation(){
+        Event ev = Tabel.getSelectionModel().getSelectedItem();
     }
 
 }
